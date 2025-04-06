91767bf0872519e145e50f0769760fe14d9317c7
EC2 Default User <ec2-user@ip-172-31-89-196.ec2.internal>
2025-04-06 22:46:29 +0000

    added profile-demo-yaml

--- alias.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-alias kgp='kubectl get pods'
-alias kdp='kubectl delete pod'
-alias kgd='kubectl get deployments'
-alias kdd='kubectl delete deployment'
-alias kgs='kubectl get svc'
-alias kds='kubectl delete svc'
-alias kga='kubectl get all'
